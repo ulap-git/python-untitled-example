ce066a6fd77d658a02d2a7178a1f73d01f00c085
Ulap <ulapi.git@gmail.com>
2020-07-07 15:50:29 +0200

    Updated readme, added main.py

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # python-untitled-example
+This is an example project to demonstrate the use of git on Ubuntu.
---
+++ main.py
@@ -0,0 +1,8 @@
+import random
+from time import sleep
+
+
+for i in range(100):
+    a = random.randint(1, 1001)
+    sleep(.5)
+    print(a)
